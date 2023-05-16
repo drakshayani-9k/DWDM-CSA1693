2b3e43a8dc106600fe5e7af72df4cbae18c4a410
Drakshayani <113888185+drakshayani-9k@users.noreply.github.com>
2023-05-16 11:59:47 +0530

    Create pie chart.R

---
+++ pie chart.R
@@ -0,0 +1,11 @@
+x <- c(21, 62, 10, 53)
+labels <- c("London", "New York", "Singapore", "Mumbai")
+
+# Give the chart file a name.
+png(file = "city.png")
+
+# Plot the chart.
+pie(x,labels)
+
+# Save the file.
+dev.off()
